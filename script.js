9111c4f53de9a2765a792396d2058237c242e007
ProgramadorNomade <94707235+ProgramadorNomade@users.noreply.github.com>
2021-11-19 14:50:12 -0300

    commit gastro
    Primeiro commit do site

---
+++ script.js
@@ -0,0 +1,11 @@
+// alert('Gastrô Food agradece a preferência!!')
+
+function modalcookie(modalcookie1) {
+    const modalcookie = document.getElementById(modalcookie1)
+    modalcookie1.classlist.add(modal-cookie-mostrar)
+}
+
+iniciamodalcookie('modal-cookie') 
+
+
+
